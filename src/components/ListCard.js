e379108f4c70cd1c8609087eba8b036336a2494a
sschrs <s_ozarslan07@outlook.com>
2023-02-17 02:36:59 +0300

    changed layout size of list card

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ListCard = (props) => {
     const { title, wordCount, link } = props;
     return (
-        <div className="col-4 p-1">
+        <div className="col-3 p-1">
             <Link to={link} style={{'textDecoration' : 'none'}}>
                 <div className="list-box bg-secondary p-3 rounded">
                     <div className="title text-light">{title}</div>
