a0c941903dea1f584e3f6748b09f04e601ced2f5
sschrs <s_ozarslan07@outlook.com>
2023-02-17 13:47:35 +0300

    configured routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/shared/Navbar";
+import NewListPage from "./pages/NewListPage";
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/new-list" element={<NewListPage/>} />
+      </Routes>
+
     </div>
   );
 }
