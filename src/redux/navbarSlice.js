f80b942d8dd35e12167fd0f778d4229d38f494a6
sschrs <s_ozarslan07@outlook.com>
2023-02-17 13:45:05 +0300

    created slices for navbar info text

--- src/redux/navbarSlice.js
+++ src/redux/navbarSlice.js
@@ -1,14 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const initialState = {
-    navbarInfoText : ""
+    navbarInfoText: undefined
 }
 
 export const navbarSlice = createSlice({
     name: "navbar",
     initialState,
     reducers:{
-        changeInfoText : (state, action) => state.navbarInfoText = action.payload
+        changeInfoText : (state, action) => void(state.navbarInfoText = action.payload)
     }
 })
 
