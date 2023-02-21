b1d678a7d83e651f517d12402b8ab38a771cfd0b
sschrs <s_ozarslan07@outlook.com>
2023-02-21 16:02:17 +0300

    router links were added

--- src/components/HomePage/ListCard.js
+++ src/components/HomePage/ListCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// show list title and link
 const ListCard = (props) => {
-    const { title, wordCount, link } = props;
+    const { title, link } = props;
     return (
-        <div className="col-3 p-1">
-            <Link to={link} style={{'textDecoration' : 'none'}}>
-                <div className="list-box bg-secondary p-3 rounded">
+        <div className="col-3 d-flex p-1">
+            <Link to={link} style={{ 'textDecoration': 'none', width: '100%'}}  >
+                <div className="list-box bg-secondary p-3 rounded" style={{height:'100%'}}>
                     <div className="title text-light">{title}</div>
-                    <small className="text-muted">{wordCount} Words</small>
                 </div>
             </Link>
         </div>
