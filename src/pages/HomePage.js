12335e3642b902670e02910befd4a6c4d360c6e4
sschrs <s_ozarslan07@outlook.com>
2023-02-21 16:00:42 +0300

    design changed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,41 +1,41 @@
-import React from "react";
+/**
+ * @author Süleyman Özarslan
+ * @version 1.0.0
+ */
+import React, { useEffect, useState } from "react";
 import ListCard from "../components/HomePage/ListCard";
+
+/**
+ * lists all word lists
+ */
 const HomePage = () => {
+    const [lists, setLists] = useState([]);
+
+    useEffect(() => {
+        getLists();
+    })
+
+    // get word lists from db and update state
+    const getLists = async () => {
+        const lists = await window.electron.getAllWordLists();
+        setLists(lists);
+    }
+
     return (
         <div className="container mt-4">
-            <div className="row">
-               <ListCard
-                title="Most Common Words in English" 
-                wordCount="2650"
-                link="/"
-               /> 
-
-<ListCard
-                title="Most Common Words in English" 
-                wordCount="2650"
-                link="/"
-               /> 
-
-<ListCard
-                title="Most Common Words in English" 
-                wordCount="2650"
-                link="/"
-               /> 
-
-<ListCard
-                title="Most Common Words in English" 
-                wordCount="2650"
-                link="/"
-               /> 
-
-<ListCard
-                title="Most Common Words in English" 
-                wordCount="2650"
-                link="/"
-               /> 
+            <div className="row equal">
+                {lists.map((wordList, index) => {
+                    return (
+                        <ListCard
+                            title={wordList.title}
+                            wordCount=""
+                            link={`/list-detail/${wordList.id}`}
+                        />
+                    )
+                })
+                }
             </div>
         </div>
     )
 }
-
 export default HomePage;
