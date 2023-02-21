92ef376fd80f701a6b8b46c3f0689cc7bbadd880
sschrs <s_ozarslan07@outlook.com>
2023-02-21 15:55:40 +0300

    edit-list was added to routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import HomePage from "./pages/HomePage";
 import Navbar from "./components/shared/Navbar";
 import NewListPage from "./pages/NewListPage";
 import ImportPage from "./pages/ImportPage";
+import ListDetailPage from "./pages/ListDetailPage";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div style={{marginTop: '70px'}}></div>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/new-list" element={<NewListPage/>} />
         <Route path="/import" element={<ImportPage />} />
+        <Route path="/list-detail/:listId" element={<ListDetailPage />} />
+        <Route path="/edit-list/:listId" element={<NewListPage />} />
       </Routes>
 
     </div>
