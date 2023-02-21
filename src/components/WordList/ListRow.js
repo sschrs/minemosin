a3e01d7abe2683be9f22e87a964b923949370661
sschrs <s_ozarslan07@outlook.com>
2023-02-21 15:54:40 +0300

    isEditable feature was added

--- src/components/WordList/ListRow.js
+++ src/components/WordList/ListRow.js
@@ -4,39 +4,41 @@ import { RiDeleteBin2Fill } from 'react-icons/ri';
 import { BsFillPencilFill } from 'react-icons/bs';
 
 
-const ListRow = (props)=>{
-    const { id, keyword, value, onDelete, onUpdate } = props
+const ListRow = (props) => {
+    const { isEditable, keyword, value, onDelete, onUpdate } = props
 
     return (
         <div className="row">
-                <div className="col-5 mt-2">
-                    <div className="card border-secondary">
-                        <div className="bg-secondary card-body">
-                            <b>{keyword}</b>
-                        </div>
+            <div className={isEditable ? 'col-5 mt-2' : 'col-6 mt-2'}>
+                <div className="card border-secondary">
+                    <div className="bg-secondary card-body">
+                        <b>{keyword}</b>
                     </div>
                 </div>
+            </div>
 
 
-                <div className="col-5 mt-2">
-                    <div className="card border-secondary">
-                        <div className="bg-secondary card-body">
-                            <b>{value}</b>
-                        </div>
+            <div className={isEditable ? 'col-5 mt-2' : 'col-6 mt-2'}>
+                <div className="card border-secondary">
+                    <div className="bg-secondary card-body">
+                        <b>{value}</b>
                     </div>
                 </div>
+            </div>
 
+            {isEditable &&
                 <div className="col-2 mt-2">
                     <div className="row">
                         <div className="col-6">
-                            <button className="btn btn-outline-warning p-3 w-100" onClick={onUpdate}><BsFillPencilFill/></button>
+                            <button className="btn btn-outline-warning p-3 w-100" onClick={onUpdate}><BsFillPencilFill /></button>
                         </div>
                         <div className="col-6">
                             <button className="btn btn-outline-danger p-3 w-100" onClick={onDelete} ><RiDeleteBin2Fill /></button>
                         </div>
                     </div>
                 </div>
-            </div>
+            }
+        </div>
     )
 }
 
--- src/pages/NewListPage.js
+++ src/pages/NewListPage.js
@@ -1,20 +1,31 @@
+/**
+ * @author Süleyman Özarslan
+ * @version 1.0.0 
+ */
 import React, { useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { changeInfoText } from "../redux/navbarSlice";
 
 import Input from "../components/shared/Input";
 import ListRow from "../components/WordList/ListRow";
 
+/**
+ * Add a new list or edit existing one
+ * if listId exists -> edit list
+ * if listId not exists -> add new list
+ */
 const NewListPage = () => {
+    const { listId } = useParams(); // get id from react-router params | if exists edit the list
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    //State
-    const [insertedListId, setInsertedListId] = useState(0);
+    
+    const [insertedListId, setInsertedListId] = useState(listId ? listId : 0); // if listId not exists (new-list) initial with 0
     const [listName, setListName] = useState("Untitled List");
     const [inputKey, setIputKey] = useState("");
     const [inputValue, setInputValue] = useState("");
-    const [wordList, setWordList] = useState([]);
+    const [keyWordMatchs, setKeyWordMatchs] = useState([]);
+    const [wordList, setWordList] = useState({});
 
     //Input refs
     const keyInputRef = useRef(null);
@@ -22,89 +33,143 @@ const NewListPage = () => {
     const listNameInputRef = useRef(null);
 
     useEffect(() => {
-        dispatch(changeInfoText("Add New List"));
-        return () => {
-            dispatch(changeInfoText(undefined));
+        listId ? dispatch(changeInfoText("Edit List")) : dispatch(changeInfoText("Add New List")) // change info text in the navbar according to listId
+        /**
+         * if listId exists get relevant records from db
+         */
+        if (listId) {
+            getSavedList();
+            getWords();
         }
-    })
+        return () => {
+            dispatch(changeInfoText(undefined)); // change info text to undefined again before unmount
+        }
+    }, [])
 
+    /**
+     * get wordList record from db according to listId
+     */
+    const getSavedList = async () => {
+        const wordListDB = await window.electron.getWordListById(listId);
+        setWordList(wordListDB);
+        listNameInputRef.current.value = wordListDB.title;
+        setListName(wordListDB.title);
+    }
+    /**
+     * get key-word matchs from db according to listId
+     */
+    const getWords = async () => {
+        const words = await window.electron.getAllKeyWordMatchs(`wordListId = ${listId}`);
+        setKeyWordMatchs(words);
+    }
+    /**
+     * update list title in case e.key is enter or tab from list name input
+     * @param {object} e - event of input
+     */
     const handleTitleUpdate = (e) => {
         if ((insertedListId != 0) && (e.key === 'Enter' || e.key === 'Tab')) {
-           updateTitle();
+            updateTitle();
         }
     }
-
+    /**
+     * add new key-word match in case e.key is enter or tab
+     * @param {object} e - event of input 
+     */
     const handleKeyDown = (e) => {
         if (e.key === 'Enter' || e.key === 'Tab') {
             addNewItem(e);
         }
     }
 
+    /**
+     * insert new list to db and returns id of new list
+     * @returns {number} - id of inserted list
+     */
     const addNewList = async () => {
         const newListData = { title: listName };
         const newListId = await window.electron.insertWordList(newListData);
-        setInsertedListId(newListId);
-        listNameInputRef.current.value = listName;
+        setInsertedListId(newListId); // update state with new list id
+        listNameInputRef.current.value = listName; // update value of list name input
         return newListId;
     }
-
+    /**
+     * insert new key-word match to db and update state
+     * @param {object} e - event of button
+     */
     const addNewItem = async (e) => {
         e.preventDefault();
         if (inputKey != "" && inputValue != "") {
-
             let listId = insertedListId;
             if (insertedListId == 0) {
                 listId = await addNewList();
             }
+            // create data object with state
             const newMatchData = {
                 wordListId: listId,
                 key: inputKey,
                 value: inputValue,
-                que: wordList.length + 1,
+                que: keyWordMatchs.length + 1,
                 testComplated: 0,
                 priority: 1,
             };
-            const insertedMatchId = await window.electron.insertKeyWordMatch(newMatchData);
+            const insertedMatchId = await window.electron.insertKeyWordMatch(newMatchData); // get id of inserted match
 
-            let newList = [...wordList];
+            // update state with new match
+            let newList = [...keyWordMatchs];
             newList.push({
                 id: insertedMatchId,
-                keyword: inputKey,
+                key: inputKey,
                 value: inputValue
             })
+            setKeyWordMatchs(newList);
 
-            setWordList(newList);
+            // clean state and inputs
             setInputValue("");
             setIputKey("");
             valueInputRef.current.value = "";
             keyInputRef.current.value = "";
         }
-        keyInputRef.current.focus();
+        keyInputRef.current.focus(); // refocus to key-input
     }
 
-    const updateTitle = ()=> {
+    /**
+     * update title of list
+     * @returns {promise} - 
+     */
+    const updateTitle = () => {
         const data = { id: insertedListId, setString: `title = \'${listName}\'` }
         return window.electron.updateWordListById(data);
     }
 
-    const deleteWord = (id)=>{
-        window.electron.deleteKeyWordMatch(id).then(()=>{
-            let newList = [...wordList];
+    /**
+     * delete word according to its id
+     * @param {number} id - id of word to delete 
+     */
+    const deleteWord = (id) => {
+        window.electron.deleteKeyWordMatch(id).then(() => {
+            let newList = [...keyWordMatchs];
             newList.splice(newList.findIndex(word => word.id === id), 1);
-            setWordList(newList);
+            setKeyWordMatchs(newList);
         });
     }
 
-    const updateWord = (word)=>{
+    /**
+     * delete a word and set input values to update it
+     * @param {object} word - word (key-word match)
+     */
+    const updateWord = (word) => {
         const { id, keyword, value } = word;
         deleteWord(id);
         keyInputRef.current.value = keyword;
         valueInputRef.current.value = value;
     }
 
-    const saveAndQuit = ()=>{
-        updateTitle().then(()=>{
-            navigate('/');
+    /**
+     * update title and navigate back 
+     */
+    const saveAndQuit = () => {
+        updateTitle().then(() => {
+            listId ? navigate(`/list-detail/${listId}`) : navigate('/');
         });
     }
 
@@ -131,12 +196,14 @@ const NewListPage = () => {
                 </div>
                 <hr className="mt-2" />
             </div>
-            {wordList.map((word, index) => {
-                return (
-                    <ListRow keyword={word.keyword} value={word.value} key={word.id} onDelete={e => deleteWord(word.id)} onUpdate={e => updateWord(word)} />
-                )
-            })
-            }
+            <div className="list-row-container">
+                {keyWordMatchs.map((word, index) => {
+                    return (
+                        <ListRow isEditable={true} keyword={word.key} value={word.value} key={word.id} onDelete={e => deleteWord(word.id)} onUpdate={e => updateWord(word)} />
+                    )
+                })
+                }
+            </div>
         </div>
     )
 }
