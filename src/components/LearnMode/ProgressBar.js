81e4d27b15e84de0375629eb5d7c40435364e79a
sschrs <s_ozarslan07@outlook.com>
2023-02-23 15:24:16 +0300

    progress bar added

--- src/components/LearnMode/ProgressBar.js
+++ src/components/LearnMode/ProgressBar.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const ProgressBar = () => {
+const ProgressBar = (props) => {
+    const { listId } = props;
+    const [percent, setPercent] = useState(0);
+    const [complatedWords, setComplatedWords] = useState(0);
+    const [totalWords, setTotalWords] = useState(0);
+
+    useEffect(()=>{
+        getProgress();
+    })
+
+    const getProgress = async () => {
+        const total = await window.electron.getCount(`wordListId = ${listId}`)
+        const complated = await window.electron.getCount(`wordListId = ${listId} AND priority = 0`)
+        setTotalWords(total);
+        setComplatedWords(complated);
+        setPercent(Math.floor(complated*100/total));
+    }
+
     return (
         <div>
-            <div className="progress mt-3" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div className="progress-bar progress-bar-striped bg-secondary" style={{ width: '25%' }}></div>
+            <div className="progress mt-3">
+                <div className="progress-bar progress-bar-striped bg-secondary" style={{ width: `${percent}%` }}></div>
             </div>
-            <small className="text-muted">25% - 120/480</small>
+            <small className="text-muted">You learned: {percent}% - {complatedWords}/{totalWords}</small>
         </div>
 
     )
