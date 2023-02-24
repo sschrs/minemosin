f6ce3795e8f7529a97aa532564208bfb8dae4376
sschrs <s_ozarslan07@outlook.com>
2023-02-25 01:01:33 +0300

    deleted assets

--- src/helpers/learnMode.js
+++ src/helpers/learnMode.js
@@ -3,6 +3,8 @@
  * @version 1.0.0
  */
 
+import levenSort from "leven-sort";
+
 /**
  * Fisher–Yates Algorithm to shuffle an array
  * @param {array} array - array will be shuffled
@@ -29,11 +31,62 @@ export const shuffleWordList = async (wordListId) => {
     const words = await window.electron.getAllKeyWordMatchs(`wordListId = ${wordListId}`);
     const shuffledArray = shuffleArray(words);
     shuffledArray.map((word, index) => {
-        console.log(word.key)
-        const data = { 
-            id : word.id,
+        const data = {
+            id: word.id,
             set: `que = ${index}`
-         };
+        };
         window.electron.updateKeyWordMatchById(data);
     });
+    await window.electron.updateWordListById({ id: wordListId, setString: 'inProgress = 1' })
+}
+
+export const resetWordList = async (wordListId) => {
+    await window.electron.updateWordListById({ id: wordListId, setString: 'inProgress = 0' });
+    await window.electron.updateKeyWordMatch({ where: `wordListId = ${wordListId}`, setString: 'testCompleted = 0, priority = 1' });
+}
+
+export const getUntestedWordByQue = async (wordListId) => {
+    const data = { where: `wordListId = ${wordListId} AND testCompleted = 0`, limit: 1 }
+    const words = await window.electron.getAllKeyWordMatchsWithLimitByQue(data);
+    if (words.length > 0) return words[0];
+    return undefined;
+}
+
+export const getQuizWordsByQue = async (wordListId) => {
+    const data = {
+        where: `wordListId = ${wordListId} AND testCompleted = 1 AND priority != 0`,
+        limit: 1
+    }
+    const words = await window.electron.getAllKeyWordMatchsWithLimitByQue(data);
+    if (words.length > 0) return words[0];
+    return undefined;
+}
+
+export const getSimilarChoices = async (wordListId, word, reverse = false) => {
+    const words = await window.electron.getAllKeyWordMatchs(`wordListId = ${wordListId}`)
+    let strings = new Array();
+    words.forEach(word => {
+        reverse ? strings.push(word.value) : strings.push(word.key);
+    });
+    let sorted;
+    reverse ? sorted = levenSort(strings, word.value) : sorted = levenSort(strings, word.key);
+    return shuffleArray(sorted.slice(0, 4));
+}
+
+export const checkQuizAnswer = (answer, correctAnswer) => {
+    const answerList = answer.split(',');
+    const correctList = correctAnswer.split(',');
+    var isCorrect = true;
+    answerList.map((value, index) => {
+        answerList[index] = value.toLowerCase().trim();
+    })
+
+    correctList.map((value, index) => {
+        correctList[index] = value.toLowerCase().trim();
+    })
+
+    answerList.forEach(word => {
+        if (!correctList.includes(word)) isCorrect = false;
+    });
+    return isCorrect;
 }
